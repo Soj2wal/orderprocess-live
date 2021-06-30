892e67f41ada2b461adc6812bafd84db7b35f611
Sojjwal <sojjwal_shrestha@hotmail.com>
2021-06-30 10:00:53 +0545

    Update settings.py

--- OrderProcess/settings.py
+++ OrderProcess/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '!b8l!n8p&t+*(lbjl8m*=!p-2#ps%y^10i#k36*h*mg4gw_kes'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['sojjwalorderprocess.herokuapp.com', '127.0.0.1:8000']
 
